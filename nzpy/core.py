5c636307c8b32dbf9ed1f996d2ab30f0c4b4113f
Arthur Varkentin <Arthur.Varkentin@gmail.com>
2021-09-28 14:23:31 +0200

    Revert last changes

--- nzpy/core.py
+++ nzpy/core.py
@@ -2067,19 +2067,19 @@ class Connection():
                 memsize = 1
                 
             if fldtype == NzTypeChar:
-                value = bytes(fieldDataP[:fldlen], self._char_varchar_encoding)
+                value = fieldDataP[:fldlen]
                 row.append(value)
                 self.log.debug("field=%d, datatype=CHAR, value=%s", cur_field+1,value)                
                 
             if fldtype == NzTypeNChar or fldtype == NzTypeNVarChar:
                 cursize  = int.from_bytes(fieldDataP[0:2], 'little') - 2
-                value = bytes(fieldDataP[2:cursize+2], self._char_varchar_encoding)
+                value = fieldDataP[2:cursize+2]
                 row.append(value)
                 self.log.debug("field=%d, datatype=%s, value=%s", cur_field+1,dataType[fldtype], value)                
                 
             if fldtype == NzTypeVarChar or fldtype == NzTypeVarFixedChar or fldtype == NzTypeGeometry or fldtype == NzTypeVarBinary or fldtype == NzTypeJson or fldtype == NzTypeJsonb or fldtype == NzTypeJsonpath:
                 cursize  = int.from_bytes(fieldDataP[0:2], 'little') - 2
-                value = bytes(fieldDataP[2:cursize+2], self._char_varchar_encoding)
+                value = fieldDataP[2:cursize+2]
                 row.append(value)
                 self.log.debug("field=%d, datatype=%s, value=%s", cur_field+1,dataType[fldtype], value)
                 
